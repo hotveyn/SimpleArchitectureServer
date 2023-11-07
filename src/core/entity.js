19de9adc9b03888eabf6af5ccaa310502d4d0646
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-11-08 00:22:43 +0300

    add insert and createAttributes property

--- src/core/entity.js
+++ src/core/entity.js
@@ -1,10 +1,12 @@
 export class Entity {
 	db;
 	tableName;
+	creationAttributes;
 	
-	constructor(db, tableName) {
+	constructor(db, tableName, creationAttributes) {
 		this.db = db;
 		this.tableName = tableName
+		this.creationAttributes = creationAttributes
 	}
 	
 	select(what, restOfScript = "") {
@@ -13,6 +15,11 @@ export class Entity {
 	}
 	
 	insert(values) {
-		const script = this.db.prepare(`INSERT INTO ${this.tableName} VALUES(${values.join(', ')})`)
+		// Пиздец бля, так вот зачем нужны орм и кверибилдеры
+		const creationAttributesValues = Object.values(this.creationAttributes);
+		let valuesString = '?, '.repeat(creationAttributesValues.length);
+		valuesString = valuesString.slice(0, valuesString.length - 2);
+		const script = this.db.prepare(`INSERT INTO ${this.tableName} (${creationAttributesValues.join(', ')}) VALUES(${valuesString})`)
+		return script.run(values);
 	}
 }
