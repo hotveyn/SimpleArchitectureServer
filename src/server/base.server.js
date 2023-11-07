02fa059d204a98329aebad170980d7e4fc746503
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-11-07 17:59:21 +0300

    change system of connect modules and http routes

--- src/server/base.server.js
+++ src/server/base.server.js
@@ -1,50 +1,61 @@
 import express from "express";
-import { baseLogger } from "../logger/base.logger.js";
+import {baseLogger} from "../logger/base.logger.js";
+import {endOfTimesMiddleware} from "../middleware/end-of-times.middleware.js";
 
 export class BaseServer {
-	server;
-	logger;
-	
-	constructor(logger) {
-		this.server = express()
-		this.logger = logger;
-	}
-	
-	async init() {
-		try {
-			const PORT = process.env.SERVER_PORT || 7123
-			this.logger.start('Starting server...')
-			await new Promise((resolve, reject) => {
-				this.server.listen(PORT, (err) => {
-					if (err) {
-						reject(err)
-					}
-					this.logger.success(`Server is listening on http://localhost:${PORT}`)
-					resolve(this.server)
-				})
-			})
-			
-			return this.server
-		} catch (e) {
-			this.logger.warn("App crashed on server init stage")
-			this.logger.error(e)
-			process.exit(1)
-		}
-	}
-	
-	registerModules(modules) {
-		modules.forEach((module) => {
-			this.registerModule(module)
-		})
-	}
-	
-	registerModule(module) {
-		try {
-			this.logger.start(`Register module ${module.name}...`)
-			this.server.use(`/${module.name}`, module.router)
-		} catch (e) {
-			this.logger.error(e)
-			process.exit(1)
-		}
-	}
+    server;
+    logger;
+
+    constructor(logger) {
+        this.server = express()
+
+        this.logger = logger;
+    }
+
+    async init() {
+        try {
+            this.logger.start('Starting server...')
+            await this.listen()
+        } catch (e) {
+            this.logger.warn("App crashed on server init stage")
+            this.logger.error(e)
+            process.exit(1)
+        }
+    }
+
+    async listen() {
+        const PORT = process.env.SERVER_PORT || 7123
+
+        await new Promise((resolve, reject) => {
+            this.server.listen(PORT, (err) => {
+                if (err) {
+                    reject(err)
+                }
+                this.logger.success(`Server is listening on http://localhost:${PORT}`)
+                resolve(this.server)
+            })
+        })
+
+    }
+
+    registerHTTPModules(modules) {
+        modules.forEach((module) => {
+            this.registerHTTPModule(module)
+        })
+    }
+
+    registerHTTPModule(module) {
+        try {
+            this.logger.start(`Registration module ${module.name}...`)
+            this.server.use(`/${module.name}`, module.router)
+        } catch (e) {
+            this.logger.error(e)
+            process.exit(1)
+        }
+    }
+
+    registerExpressAddon(addon) {
+        this.server.use(addon)
+    }
+
 }
