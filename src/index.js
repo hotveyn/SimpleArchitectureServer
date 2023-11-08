83506de7e5dce68fd6b37b793ed939c310922f9a
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-11-08 21:57:53 +0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {AuthModule} from "./http/auth/auth.module.js";
 import express from "express";
 import 'dotenv/config'
 import {endOfTimesMiddleware} from "./middleware/end-of-times.middleware.js";
+import cors from "cors";
 
 
 async function bootstrap() {
@@ -17,6 +18,12 @@ async function bootstrap() {
 	])
 
 	const server = new BaseServer(baseLogger);
+	server.registerExpressAddon(cors({
+		origin:["http://localhost:5173"],
+		credentials: true,
+		SameSite: false,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+	}))
 	server.registerExpressAddon(express.json())
 	server.registerHTTPModules([
 		new UserModule(),
